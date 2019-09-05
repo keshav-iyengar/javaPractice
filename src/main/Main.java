f578830a8483b663057212be8d590f09821fd250
Keshav Iyengar <keshav.iyengar@scifusion360.com>
2019-09-05 11:29:30 -0400

    Fibonnaci.

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,6 +4,36 @@ public class Main {
 
 	public static void main(String[] args) {
 
+		//doFibRecurs(13, 0, 1);
+		int i = fibonacci(7);
+		System.out.println(i);
+
+	}
+
+	public static void doFib(int limit) {
+
+		int val1 = 0;
+		int val2 = 1;
+		int value = 0;
+
+		System.out.print(1 + " ");
+		for(int i = 1; i <= limit; i++) {
+
+			value = val1 + val2;
+			if(value > limit) break;
+			System.out.print(value + " ");
+			val1 = val2;
+			val2 = value;
+		}
+
+	}
+
+	public static int fibonacci(int number) {
+		if(number == 1 || number == 2) {
+			return 1;
+		}
+
+		return fibonacci(number - 1) + fibonacci(number - 2); //tail recursion
 	}
 
 }
